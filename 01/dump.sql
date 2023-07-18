4301e64f7d0f823f1f847c34db099d1910577548
Daniel Pinheiro Machado <dyelzim@gmail.com>
2023-07-18 20:34:29 -0300

    finished  4, 5, 6 challenges

--- 01/dump.sql
+++ 01/dump.sql
@@ -11,3 +11,13 @@ CREATE TABLE authors (
 -- insert author
 INSERT INTO authors (name, age)
 VALUES ("Guido Cerqueira", 32);
+
+-- create table books
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  gender VARCHAR(100),
+  publisher VARCHAR(100),
+  publication_date DATE,
+  author_id INTEGER REFERENCES authors(id)
+);
--- 01/server.js
+++ 01/server.js
@@ -8,6 +8,54 @@ const app = express();
 app.use(express.json());
 
 //ROUTES
+//GET
+app.get("/author/:id", async (req, res) => {
+  const idAuthor = req.params.id;
+
+  try {
+    const result = await pool.query(
+      "SELECT authors.id, authors.name, authors.age, books.id as book_id, books.name as book_name, books.gender, books.publisher, books.publication_date FROM authors LEFT JOIN books ON authors.id = books.author_id WHERE authors.id = $1",
+      [idAuthor]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "Author not found" });
+    }
+
+    const author = {
+      id: result.rows[0].id,
+      name: result.rows[0].name,
+      age: result.rows[0].age,
+      books: [],
+    };
+
+    for (const row of result.rows) {
+      if (row.book_id) {
+        const book = {
+          id: row.book_id,
+          name: row.book_name,
+          gender: row.gender,
+          publisher: row.publisher,
+          publication_date: row.publication_date,
+        };
+        author.books.push(book);
+      }
+    }
+
+    if (author.books.length === 0) {
+      return res.status(404).json({
+        message: "Book not found.",
+      });
+    }
+
+    res.json(author);
+  } catch (error) {
+    console.error("Error when searching for author:", error);
+    res.status(500).json({ message: "Error when searching for author." });
+  }
+});
+
+app.get("/book", (req, res) => {});
 
 //POST
 app.post("/author", async (req, res) => {
@@ -41,23 +89,34 @@ app.post("/author", async (req, res) => {
   }
 });
 
-//GET
-app.get("/author/:id", async (req, res) => {
-  const id = req.params.id;
-  console.log(id);
-  try {
-    const querySelectedAuthor = `
-    SELECT * FROM authors
-    WHERE id = $1;
-  `;
-    const selectedAuthor = (await pool.query(querySelectedAuthor, [id])).rows;
+app.post("/author/:id/book", async (req, res) => {
+  const idAuthor = req.params.id;
+  const { name, gender, publisher, publication_date } = req.body;
 
-    if (selectedAuthor.length === 0) {
-      return res.status(404).json({
-        message: "Author not found.",
-      });
-    }
-    return res.json(selectedAuthor);
+  if (!name) {
+    return res.status(400).json({
+      message: "The name field is required.",
+    });
+  }
+
+  try {
+    const bookRegister = await pool.query(
+      `
+        INSERT INTO books 
+        (name, gender, publisher, publication_date, author_id) 
+        VALUES ($1, $2, $3, $4, $5)
+      `,
+      [name, gender, publisher, publication_date, idAuthor]
+    );
+
+    const books = (await pool.query("SELECT * FROM books;")).rows;
+
+    const book = {
+      id: books[books.length - 1].id,
+      ...req.body,
+    };
+
+    return res.json(book);
   } catch (error) {
     console.log(error.message);
   }
